d2a913656ae068c4a9819611669c43bc5e183a5c
guizzzo <09087473907@edu.udesc.br>
2025-03-31 21:43:52 -0300

    Criação de uma interface interativa, perguntando nome e idade e imprimindo

--- src/teste/TestaOlaMundo.java
+++ src/teste/TestaOlaMundo.java
@@ -1,8 +1,20 @@
 package teste;
 
+import java.util.Scanner;
+
 public class TestaOlaMundo {
     public static void main(String[] args) {
         OlaMundo warudo = new OlaMundo();
         warudo.imprimirDados();
+
+        Scanner scanDados = new Scanner(System.in);
+        System.out.print("Digite seu nome: ");
+        String nome = scanDados.nextLine();
+
+        System.out.print("Digite sua idade: ");
+        int valor = scanDados.nextInt();
+
+        warudo.imprimir(nome, valor);
+
     }
 }
